ad5f2baf5cf1b1cd10b2d40689e9842a562653fd
David Garner <david.garner@trojans.dsu.edu>
2026-05-20 07:49:56 -0500

    Changes to volatility path.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-VOLATILITY_PATH = "vol.py"
+VOLATILITY_PATH = "C:\\Users\\david\\Documents\\CSC842\\volatility3\\vol.py"
 
 MEMORY_IMAGE = "memory.mem"
 
@@ -8,6 +8,7 @@ DUMP_DIR = "dumps"
 
 VOL_PLUGINS = [
     "windows.pslist",
+    "windows.psscan",
     "windows.malfind",
     "windows.dlllist",
     "windows.netscan"
