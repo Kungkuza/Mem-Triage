cb658a8060ee6c727f1fb15452daa0dd9a4a007f
David Garner <david.garner@trojans.dsu.edu>
2026-06-02 07:40:41 -0500

    CContinuation of cuts to code length.

--- config.py
+++ config.py
@@ -24,29 +24,3 @@ def get_unique_dump_dir():
     unique_dir = os.path.join(BASE_DIR, f"dumps_{timestamp}")
     os.makedirs(unique_dir, exist_ok=True)
     return unique_dir os
-from datetime import datetime
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-VOLATILITY_PATH = r"C:\Users\david\Documents\CSC842\volatility3\vol.py"
-CAPA_PATH = r"capa"
-
-VOL_PLUGINS = [
-    "windows.pslist",
-    "windows.psscan",
-    "windows.malfind",
-    "windows.dlllist",
-    "windows.netscan"
-]
-
-DUMP_PLUGINS = [
-    "windows.malfind"
-]
-
-def get_unique_dump_dir():
-    
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    unique_dir = os.path.join(BASE_DIR, f"dumps_{timestamp}")
-    os.makedirs(unique_dir, exist_ok=True)
-    return unique_dir
